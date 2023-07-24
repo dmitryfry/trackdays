ead225f399eb42ac53e4cdea13aeed0cca943810
Kirilenko <n@kirilenko.com>
2023-07-24 11:09:37 +0400

    feat: add gem tailwindcss-rails

---
+++ config/tailwind.config.js
@@ -0,0 +1,36 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// const defaultTheme = require('tailwindcss/defaultTheme')
+//
+// module.exports = {
+//   content: [
+//     './public/*.html',
+//     './app/helpers/**/*.rb',
+//     './app/javascript/**/*.js',
+//     './app/views/**/*.{erb,haml,html,slim}'
+//   ],
+//   theme: {
+//     extend: {
+//       fontFamily: {
+//         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+//       },
+//     },
+//   },
+//   plugins: [
+//     require('@tailwindcss/forms'),
+//     require('@tailwindcss/aspect-ratio'),
+//     require('@tailwindcss/typography'),
+//     require('@tailwindcss/container-queries'),
+//   ]
+// }
